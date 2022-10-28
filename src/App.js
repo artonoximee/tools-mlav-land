2ae130ab71b696b9696dd347726f4bc7ead52ddd
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-28 14:01:37 +0200

    added code from previous project for login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./components/auth/PrivateRoute";
+import { AuthProvider } from "./contexts/AuthContext";
+
+import "./App.css";
+
+import Login from "./components/auth/Login";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <AuthProvider>
+      <Router>
+        <div className="container-fluid text-bg-dark">
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={ <Login /> } />
+              <Route path="/dashboard" element={ <PrivateRoute><Dashboard /></PrivateRoute> } />
+              <Route path="*" element={ <h1>Error</h1> } />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
---
+++ src/components/auth/Login.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useAuth } from "../../contexts/AuthContext";
+
+function Login() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { logIn } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  async function submitLogIn(data) {
+    try {
+      setError("");
+      setLoading(true);
+      await logIn(data.email, data.password);
+      navigate("/dashboard");
+    } catch (err) {
+      setError("Échec de la connexion");
+    }
+    setLoading(false);
+  }
+
+  return (
+    <div className="row justify-content-center top-margin">
+      <div className="col-lg-3 col-md-12">
+
+        <h3 className="mb-5">Connexion</h3>
+
+        { error && <div className="alert alert-danger mt-1">{ error }</div> }
+
+        <label htmlFor="email" className="form-label mt-1"><i className="fa-solid fa-envelope text-primary"></i> Email</label>
+        <input 
+          type="email"
+          id="email"
+          className={ `form-control text-bg-dark border-secondary ${ errors.email && "is-invalid border-danger" }` }
+          placeholder="Adresse email"
+          { ...register("email", { required: true, pattern: /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i }) }
+        />
+        { errors.email && <div className="form-text text-danger">Merci de renseigner votre adresse email pour vous connecter</div> }
+
+        <label htmlFor="password" className="form-label mt-3"><i className="fa-sharp fa-solid fa-key text-primary"></i> Mot de passe</label>
+        <input 
+          type="password"
+          id="password"
+          className={ `form-control text-bg-dark border-secondary ${ errors.password && "is-invalid border-danger" }` }
+          placeholder="Mot de passe"
+          { ...register("password", { required: true, minLength: 6 }) }
+        />
+        { errors.password && <div className="form-text text-danger">Votre mot de passe doit au minimum contenir 6 caractères.</div> }
+        
+      
+        <div className="d-grid gap-2">
+          <button className={ `btn mt-5 btn-outline-primary` } onClick={ handleSubmit(submitLogIn) } disabled={ loading } type="submit">Connexion</button>
+        </div>
+
+        <div className="d-grid gap-2">
+          <Link to="/forgot-password" className="text-center text-secondary mt-3">Mot de passe oublié ?</Link>
+        </div>
+
+        {/* <div className="d-grid gap-2">
+          <Link to="/signup" className="text-center text-secondary mt-3">Pas encore de compte ? Créez-en un ici</Link>
+        </div> */}
+
+        <div className=" bottom-margin"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
