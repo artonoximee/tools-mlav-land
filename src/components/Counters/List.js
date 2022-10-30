510460e2109d01aedf0af0ee74fa56753125bb12
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-30 11:38:46 +0100

    added chart in counter list

--- src/components/Counters/List.js
+++ src/components/Counters/List.js
@@ -4,17 +4,22 @@ import { db } from "../../config/firebase";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { useAuth } from "../../contexts/AuthContext";
 
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+
 import ListItem from "./ListItem";
 import sortByCreationDate from "../../helpers/sortByCreationDate";
+
+import "./List.css";
 
 function List() {
   const { currentUser } = useAuth();
   const [projects, setProjects] = useState();
-  const [reload, setReload] = useState(false);
+  const [counters, setCounters] = useState();
 
   useEffect(() => {
     getProjects()
-  }, [reload])
+    getCounters()
+  }, [])
 
   async function getProjects() {
     const q = query(collection(db, "projects"), where("userId", "==", currentUser.uid));
@@ -27,19 +32,73 @@ function List() {
     setProjects(arr);
   }
 
+  async function getCounters() {
+    const q = query(collection(db, "counters"));
+    const querySnapshot = await getDocs(q);
+    const arr = []
+    querySnapshot.forEach((doc) => {
+      arr.push(doc.data())
+    });
+    setCounters(arr);
+  }
+
+  function sumHours(projectId, counters) {
+    let sumHours = 0;
+    counters.forEach(counter => {
+      if (counter.projectId === projectId) {
+        sumHours += Number(counter.time)
+      }
+    })
+    return sumHours
+  }
+
+  function hoursToDays(hours) {
+    return Math.round((hours / 8) * 10) / 10
+  }
+
+  function formatDataForChart(projects, counters) {
+    let data = []
+    projects.forEach(project => {
+      const projectName = project.name.substring(0,10);
+      const projectDays = hoursToDays(sumHours(project.id, counters));
+      data.push({name: projectName, days: projectDays});
+    })
+    return data;
+  }
+
   return (
     <>
       <h4>⏱️ Compteur</h4>
       <hr />
       <Link to="/counters/new" className="btn btn-outline-primary w-100 mb-5">Ajouter un nouveau temps</Link>
+      {
+        projects && counters &&
+        <ResponsiveContainer width="100%" height="50%">
+          <BarChart
+            data={formatDataForChart(projects, counters)}
+            margin={{
+              top: 0,
+              right: 0,
+              left: 0,
+              bottom: 40,
+            }}
+          >
+            <XAxis dataKey="name" textAnchor="end" tick={{ angle: -45 }} />
+            <YAxis />
+            <Tooltip viewBox={{ x: 0, y: 0, width: 400, height: 400 }} />
+            <Bar dataKey="days" fill="#0b6efd" />
+          </BarChart>
+        </ResponsiveContainer>
+      }
 
-      <div className="list-group">
+      <div className="list-group mt-5">
         {
-          projects &&
+          projects && counters &&
           projects.map((project) => (
           <ListItem 
             key={ project.id } 
             project={ project }
+            hours={ sumHours(project.id, counters) }
           />
           ))
         }
--- src/components/Counters/ListItem.js
+++ src/components/Counters/ListItem.js
@@ -1,29 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { db } from "../../config/firebase";
-import { collection, query, where, getDocs } from "firebase/firestore";
-import { useAuth } from "../../contexts/AuthContext";
+import React from "react";
 
 function ListItem(props) {
-  const [hoursSpent, setHoursSpent] = useState(0);
-  const [daysSpent, setDaysSpent] = useState(0);
-  const { id, name } = props.project
+  const { name } = props.project;
+  const { hours } = props;
 
-  useEffect(() => {
-    getCounters()
-  }, [])
-
-  useEffect(() => {
-    setDaysSpent(Math.round((hoursSpent / 8) * 10) / 10)
-  }, [hoursSpent])
-
-  async function getCounters() {
-    const q = query(collection(db, "counters"), where("projectId", "==", id));
-    const querySnapshot = await getDocs(q);
-    const arr = []
-    querySnapshot.forEach((doc) => {
-      arr.push(doc.data())
-      setHoursSpent(prev => prev + Number(doc.data().time))
-    });
+  function hoursToDays(hours) {
+    return Math.round((hours / 8) * 10) / 10
   }
 
   return (
@@ -33,10 +15,10 @@ function ListItem(props) {
           <h5>{ name }</h5>
         </div>
         <div className="col-lg-2">
-          <small className="badge rounded-pill text-bg-secondary float-end">{ hoursSpent } h</small>
+          <small className="badge rounded-pill text-bg-secondary float-end">{ hours } h</small>
         </div>
         <div className="col-lg-2">
-          <small className="badge rounded-pill text-bg-primary float-end">{ daysSpent } jour{ daysSpent > 1 ? "s" : "" }</small>
+          <small className="badge rounded-pill text-bg-primary float-end">{ hoursToDays(hours) } jour{ hoursToDays(hours) > 1 ? "s" : "" }</small>
         </div>
       </div>
     </div>
